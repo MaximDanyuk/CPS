cfd22e4fa8d4d6ba9dfb5a8dd56babe8c4bb530e
Maxim Danyuk <MaximDanyuk@yandex.ru>
2022-11-12 10:12:29 +0300

    Исправить ошибки

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,54 @@
 const brandsButtonMore = document.querySelector(".brands__more");
 const servicesButton = document.querySelector(".services__button");
 
-function showMore(
+function showMore({
   hiddenElementsSelector,
   visibleElementsSelector,
-  buttonArrowSelector
-) {
+  buttonArrowSelector,
+  servicesIconSelector,
+  buttonTextSelector,
+  newButtonText,
+  oldButtonText,
+}) {
+  /// Находим все скрытые элементы
   const hiddenItems = Array.from(
     document.querySelectorAll(hiddenElementsSelector)
   );
-
-  hiddenItems.forEach((elem) => elem.classList.toggle(visibleElementsSelector));
-
+  /// Находим текст кнопки
+  const buttonText = document.querySelector(buttonTextSelector);
+  /// Находим стрелку у кнопки
   const buttonMoreArrow = document.querySelector(buttonArrowSelector);
-  buttonMoreArrow.classList.toggle("services__icon_rotate");
+  /// Проходимся по всем скрытым и добавляем класс видимости
+  hiddenItems.forEach((elem) => elem.classList.toggle(visibleElementsSelector));
+  /// Переворачиваем стрелку
+  buttonMoreArrow.classList.toggle(servicesIconSelector);
+  /// Если стрелка перевернутая, т е кнопку нажали, текст меняем на текст открытой кнопки
+  /// Если класса нет, т е нажали повторно- возвращаем старый текст
+  buttonMoreArrow.classList.contains(servicesIconSelector)
+    ? (buttonText.textContent = newButtonText)
+    : (buttonText.textContent = oldButtonText);
 }
 
 brandsButtonMore.addEventListener("click", () =>
-  showMore(".brands__item_invisible", "brands__item_visible", ".brands__icon")
+  showMore({
+    hiddenElementsSelector: ".section__item_invisible",
+    visibleElementsSelector: "section__item_visible",
+    buttonArrowSelector: ".brands__icon",
+    servicesIconSelector: "services__icon_rotate",
+    buttonTextSelector: ".brands__link-text",
+    newButtonText: "Скрыть",
+    oldButtonText: "Показать все",
+  })
 );
 
 servicesButton.addEventListener("click", () =>
-  showMore(".services__text", "services__text_visible", ".services__icon")
+  showMore({
+    hiddenElementsSelector: ".section__text-box_hidden",
+    visibleElementsSelector: "section__text_visible",
+    buttonArrowSelector: ".services__icon",
+    servicesIconSelector: "services__icon_rotate",
+    buttonTextSelector: ".services__link-text",
+    newButtonText: "Скрыть",
+    oldButtonText: "Показать все",
+  })
 );
